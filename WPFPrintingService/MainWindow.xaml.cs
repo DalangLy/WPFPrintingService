0dfdc787566521ef2565717bb54a058ee4bc721e
Dalang Ly <49807152+DalangLy@users.noreply.github.com>
2022-05-28 09:06:18 +0700

    Add project files.

---
+++ WPFPrintingService/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebSocketSharp.Server;
+using WebSocketSharp.Net;
+
+namespace WPFPrintingService
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private WebSocketServer? _webSocketServer;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(_webSocketServer == null)
+            {
+                _initializeWebSocketServer();
+            }
+        }
+
+        private void _initializeWebSocketServer()
+        {
+            _webSocketServer = new WebSocketServer("ws://172.30.160.1:8000");
+
+            //add listener
+
+        }
+
+        private void _startWebSocketServer()
+        {
+            if (_webSocketServer == null) return;
+
+            //check if server is not start
+
+            _webSocketServer.Start();
+        }
+
+        private void _stopWebSocketServer()
+        {
+            if (_webSocketServer == null) return;
+
+            //check if server is running
+
+            //stop the server
+            _webSocketServer.Stop();
+        }
+    }
+}

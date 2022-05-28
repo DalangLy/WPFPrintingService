96980e8c5109041027e2506f96a770c8bbdb3ae3
Dalang Ly <49807152+DalangLy@users.noreply.github.com>
2022-05-28 09:16:18 +0700

    add websocket server

--- WPFPrintingService/MainWindow.xaml.cs
+++ WPFPrintingService/MainWindow.xaml.cs
@@ -1,19 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using WebSocketSharp.Server;
-using WebSocketSharp.Net;
 
 namespace WPFPrintingService
 {
@@ -38,7 +24,11 @@ namespace WPFPrintingService
             _webSocketServer = new WebSocketServer("ws://172.30.160.1:8000");
 
             //add listener
+            _webSocketServer.AddWebSocketService<WebSocketServerListener>("/", () => new WebSocketServerListener(
+                () => {
 
+                }
+            ));
         }
 
         private void _startWebSocketServer()
---
+++ WPFPrintingService/WebSocketServerListener.cs
@@ -0,0 +1,45 @@
+﻿using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WPFPrintingService
+{
+    internal delegate void OnOpenCallBack();
+    internal class WebSocketServerListener : WebSocketBehavior
+    {
+        private OnOpenCallBack _onOpenCallBack;
+
+        public WebSocketServerListener(OnOpenCallBack onOpenCallBack)
+        {
+            this._onOpenCallBack = onOpenCallBack;
+        }
+
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+            System.Console.WriteLine(Context);
+            System.Console.WriteLine(Sessions);
+            System.Console.WriteLine(State);
+            System.Console.WriteLine(StartTime);
+            System.Console.WriteLine(Protocol);
+            System.Console.WriteLine(OriginValidator);
+            System.Console.WriteLine(ID);
+            System.Console.WriteLine(EmitOnPing);
+            this._onOpenCallBack();
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            base.OnMessage(e);
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            base.OnError(e);
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            base.OnClose(e);
+        }
+    }
+}

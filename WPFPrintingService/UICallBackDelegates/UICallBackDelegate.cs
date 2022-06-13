490070385d5c45223cf9b5306b17d25eeec4e45d
Dalang Ly <49807152+DalangLy@users.noreply.github.com>
2022-06-13 16:56:43 +0700

    make installation file

--- WPFPrintingService/UICallBackDelegates/UICallBackDelegate.cs
+++ WPFPrintingService/UICallBackDelegates/UICallBackDelegate.cs
@@ -4,11 +4,11 @@ namespace WPFPrintingService.UICallBackDelegates
 {
     public delegate void VoidCallBack(object source, EventArgs args);
 
-    public delegate void OnPrintResponse(object source, EventArgs args, string status);
-    public delegate void OnSendToEveryone(object source, EventArgs args, string message);
-    public delegate void OnSendToServer(object source, EventArgs args);
+    internal delegate void OnPrintResponse(object source, EventArgs args, string status);
+    internal delegate void OnSendToEveryone(object source, EventArgs args, string message);
+    internal delegate void OnSendToServer(object source, EventArgs args);
 
-    public delegate void OnOpenCallBack(object source, EventArgs args, string clientId, string clientIp, string clientName);
-    public delegate void onMessageCallBack(object source, EventArgs args, string clientId, string clientName, string message, OnPrintResponse onPrintResponse, OnSendToServer onSendToServer, OnSendToEveryone onSendToEveryone);
-    public delegate void OnCloseCallBack(object source, EventArgs args, string clientIp);
+    internal delegate void OnOpenCallBack(object source, EventArgs args, string clientId, string clientIp, string clientName);
+    internal delegate void onMessageCallBack(object source, EventArgs args, string clientId, string clientName, string message, OnPrintResponse onPrintResponse, OnSendToServer onSendToServer, OnSendToEveryone onSendToEveryone);
+    internal delegate void OnCloseCallBack(object source, EventArgs args, string clientIp);
 }

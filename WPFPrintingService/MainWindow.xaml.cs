0017c5354fce978fbaa8258635f36852224ae528
Dalang Ly <49807152+DalangLy@users.noreply.github.com>
2022-06-30 17:02:17 +0700

    fixed bugs

--- WPFPrintingService/MainWindow.xaml.cs
+++ WPFPrintingService/MainWindow.xaml.cs
@@ -148,4 +148,14 @@ namespace WPFPrintingService
 
         #endregion
     }
+
+    public class NotEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return string.IsNullOrWhiteSpace((value ?? "").ToString())
+                ? new ValidationResult(false, "Field is required.")
+                : ValidationResult.ValidResult;
+        }
+    }
 }

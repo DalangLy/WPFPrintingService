8d950ba0584f0e70489539e074066901218c5d49
Dalang Ly <49807152+DalangLy@users.noreply.github.com>
2022-07-01 15:54:14 +0700

    fixed bugs

---
+++ WPFPrintingService/Converter/BaseValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace WPFPrintingService
+{
+    public abstract class BaseValueConverter<TCHILD> : MarkupExtension, IValueConverter
+        where TCHILD : BaseValueConverter<TCHILD>,new()
+    {
+        private TCHILD? value;
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return value??(value = new TCHILD());
+        }
+    }
+}
---
+++ WPFPrintingService/Converter/ConvertBooleanToYesNo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFPrintingService
+{
+    public class ConvertBooleanToYesNo : BaseValueConverter<ConvertBooleanToYesNo>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolValue = (bool)value;
+
+            return boolValue?"Yes":"No";
+        }
+    }
+}

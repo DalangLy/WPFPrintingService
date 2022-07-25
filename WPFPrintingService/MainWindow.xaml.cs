5b9ae185902492a6ea13d4ce602954ff5c0f893f
Dalang Ly <49807152+DalangLy@users.noreply.github.com>
2022-07-25 17:18:48 +0700

    done

--- WPFPrintingService/MainWindow.xaml.cs
+++ WPFPrintingService/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Globalization;
 using MaterialDesignThemes.Wpf;
+using System.IO;
+using System.Text.Json;
 
 namespace WPFPrintingService
 {
@@ -45,6 +47,29 @@ namespace WPFPrintingService
             bool isExit = (bool) eventArgs.Parameter;
             if (isExit)
                 _shutdownThisApplication();
+        }
+
+        private void btnDownloadJsonTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Print Template"; // Default file name
+            dlg.DefaultExt = ".json"; // Default file extension
+            dlg.Filter = "Json File (.json)|*.json"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+                string jsonTemplate = "{ 'printTemplateLayout': { 'paddingTop': 0, 'paddingRight': 0, 'paddingBottom': 0, 'paddingLeft': 0, 'rowGap': 5, 'paperWidth': 500, 'paperBackground': 'transparent', 'fontSize': 12, 'fontFamily':'aril', 'foreground': 'black', 'rows': [ { 'row': { 'rowMarginTop': 0, 'rowMarginRight': 0, 'rowMarginBottom': 0, 'rowMarginLeft': 0, 'rowPaddingTop': 0, 'rowPaddingRight': 0, 'rowPaddingBottom': 0, 'rowPaddingLeft': 0, 'rowBorderTop': 0, 'rowBorderRight': 0, 'rowBorderBottom': 0, 'rowBorderLeft': 0, 'rowBackground': 'blue', 'rowHeight': 0, 'columnVerticalAlign': 'stretch', 'columnHorizontalAlign': 'stretch', 'columns': [ { 'column': { 'content': 'FPTP', 'contentType': 'text', 'qrCodeLogo': '', 'bold': true, 'foreground': 'black', 'fontSize': 22, 'fontFamily': 'Aril', 'contentWidth': 0, 'contentHeight': 0, 'contentHorizontalAlign': 'center', 'contentVerticalAlign': 'center', 'columnBackground': 'gray', 'columnHorizontalAlign': 'stretch', 'columnVerticalAlign': 'stretch', 'columnMarginTop': 0, 'columnMarginRight': 0, 'columnMarginBottom': 0, 'columnMarginLeft': 0, 'columnWidth': 0, 'columnHeight': 0, 'columnPaddingTop': 0, 'columnPaddingRight': 0, 'columnPaddingBottom': 0, 'columnPaddingLeft': 0, 'columnBorderTop': 0, 'columnBorderRight': 0, 'columnBorderBottom': 0, 'columnBorderLeft': 0, 'colSpan': 0, 'rowSpan': 0 } } ] } } ] } }";
+                PrintTemplateLayoutModel? layoutModel = PrintTemplateLayoutModel.FromJson(jsonTemplate);
+                string json = JsonSerializer.Serialize(layoutModel);
+                File.WriteAllText(filename, json);
+
+            }
         }
     }
 

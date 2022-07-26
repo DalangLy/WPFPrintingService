84942851fc40d83c1c9fa7e66625e8d8112e324e
Dalang Ly <49807152+DalangLy@users.noreply.github.com>
2022-07-26 11:31:26 +0700

    clean code

--- WPFPrintingService/MainWindow.xaml.cs
+++ WPFPrintingService/MainWindow.xaml.cs
@@ -113,37 +113,7 @@ namespace WPFPrintingService
         }
     }
 
-    public class AgeRangeRule : ValidationRule
-    {
-        public int Min { get; set; }
-        public int Max { get; set; }
 
-        public AgeRangeRule()
-        {
-        }
-
-        public override System.Windows.Controls.ValidationResult Validate(object value, CultureInfo cultureInfo)
-        {
-            int age = 0;
-
-            try
-            {
-                if (((string)value).Length > 0)
-                    age = Int32.Parse((String)value);
-            }
-            catch (Exception e)
-            {
-                return new System.Windows.Controls.ValidationResult(false, $"Illegal characters or {e.Message}");
-            }
-
-            if ((age < Min) || (age > Max))
-            {
-                return new System.Windows.Controls.ValidationResult(false,
-                  $"Please enter an age in the range: {Min}-{Max}.");
-            }
-            return System.Windows.Controls.ValidationResult.ValidResult;
-        }
-    }
     [ValueConversion(typeof(bool), typeof(bool))]
     public class InverseBooleanConverter : IValueConverter
     {
